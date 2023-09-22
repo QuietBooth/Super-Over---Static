4ef517df5ad1f1b5260a18aa5d9cc71e58671810
Nivaash <nidishnivash@gmail.com>
2023-09-22 22:03:42 +0530

    done

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //strike button
+
 var strikeButton = document.querySelector("#strike");
+
 //reset button
+
 var resetButton = document.querySelector("#reset");
+
 //score tags
+
 var team1score_tag = document.getElementById("score-team1");
 var team2score_tag = document.getElementById("score-team2");
+
 //wicket tags
+
 var team1Wicket_tag = document.getElementById("wicket-team1");
 var team2Wicket_tag = document.getElementById("wicket-team2");
+
 //audio variables
+
 var strikeAudio = new Audio("http://bit.ly/so-ball-hit");
 var gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
+
 //variables to keep track of game
+
 var team1Score = 0
 var team2Score = 0
 
@@ -20,17 +31,22 @@ var team2Wickets = 0
 
 var team1BallsFaced = 0
 var team2BallsFaced = 0
+
 var turn = 1
 
 var possibleOutcomes = [0,1,2,3,4,6,"W"] 
 strikeButton.addEventListener("click", strikeButtonClicked);
 
 function strikeButtonClicked(){
+
         //audio play
+
         strikeAudio.pause() //pause the previous playing audio
         strikeAudio.currentTime=0; //bring the time to 0
         strikeAudio.play()
+
         //choosing random value
+
         var randomness = Math.random()
         var random1 = randomness * possibleOutcomes.length
         var randomIndex = Math.floor(random1)
@@ -40,7 +56,9 @@ function strikeButtonClicked(){
             team2BallsFaced++;
             var ball = document.querySelector(`#team-2-super-over div:nth-child(${team2BallsFaced})`)
             ball.innerHTML = randomValue
+
         //if the ball is wicket, then increase wicket count, else add runs
+
         if(randomValue=="W"){
             team2Wickets++
         }else{
@@ -58,6 +76,7 @@ function strikeButtonClicked(){
 
         
         //India Batting
+        
         if(turn==1){
             team1BallsFaced++;
             var ball = document.querySelector(`#team-1-superover div:nth-child(${team1BallsFaced})`)
